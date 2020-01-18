be8f729c0e150c5a10a261c8e4535cb803938ff8
Joseph Trubenstein <josephtrubenstein@gmail.com>
2020-01-18 18:43:13 +0900

    first

---
+++ src/components/CollapseCard.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { blue } from "@material-ui/core/colors";
+// import FavoriteIcon from "@material-ui/icons/Favorite";
+// import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: blue[500]
+  },
+  bigKanji: {
+    fontSize: "4rem",
+    textAlign: "center"
+  }
+}));
+
+export default function CollapseCard(props) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {props.kanji}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.jlpt}
+        subheader={`radical: ${props.radical} strokes: ${props.strokes}`}
+      />
+
+      <Typography className={classes.bigKanji}>{props.kanji}</Typography>
+
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.type}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          {/* <FavoriteIcon /> */}
+        </IconButton>
+        <IconButton aria-label="shar">{/* <ShareIcon /> */}</IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography variant="h5" color="textSecondary" component="h3">
+            Meaning
+          </Typography>
+          <Typography paragraph>{props.meaning.replace(/;/g, ", ")}</Typography>
+          <Typography variant="h5" color="textSecondary" component="h3">
+            Onyomi
+          </Typography>
+          <Typography paragraph>{props.on}</Typography>
+          <Typography variant="h5" color="textSecondary" component="h3">
+            Kunyomi
+          </Typography>
+          <Typography paragraph>{props.kun}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
